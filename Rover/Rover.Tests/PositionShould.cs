058746c122b2bcc9d69e1e1535e7ac90be616a9e
Konrad Korus <konradkorus@gmail.com>
2018-05-13 09:57:25 +0100

    Add test cases for Position turning left & right.

--- Rover/Rover.Tests/PositionShould.cs
+++ Rover/Rover.Tests/PositionShould.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System.Collections.Generic;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace Rover.Tests
@@ -11,13 +12,13 @@ namespace Rover.Tests
         {
             // Arrange
             var position = new Position(new Point(1, 1), Direction.North);
-            var expectedEndPosition = new Position(new Point(1, 2), Direction.North);
+            var expectedNextPosiiton = new Position(new Point(1, 2), Direction.North);
 
             // Act
             var nextPosition = position.Forward();
 
             // Assert
-            nextPosition.Should().BeEquivalentTo(expectedEndPosition);
+            nextPosition.Should().BeEquivalentTo(expectedNextPosiiton);
         }
 
         [Test]
@@ -25,41 +26,47 @@ namespace Rover.Tests
         {
             // Arrange
             var position = new Position(new Point(1, 1), Direction.North);
-            var expectedEndPosition = new Position(new Point(1, 0), Direction.North);
+            var expectedNextPosiiton = new Position(new Point(1, 0), Direction.North);
 
             // Act
             var nextPosition = position.Backward();
 
             // Assert
-            nextPosition.Should().BeEquivalentTo(expectedEndPosition);
+            nextPosition.Should().BeEquivalentTo(expectedNextPosiiton);
         }
 
-        [Test]
-        public void Change_Direction_When_Turining_Left()
+        [TestCaseSource(nameof(TurnLeftTestCases))]
+        public void Change_Direction_When_Turning_Left(Position position, Position expectedNextPosiiton)
         {
-            // Arrange
-            var position = new Position(new Point(0, 0), Direction.North);
-            var expectedEndPosition = new Position(new Point(0, 0), Direction.West);
-
-            // Act
-            var nextPosition = position.TurnLeft();
-
-            // Assert
-            nextPosition.Should().BeEquivalentTo(expectedEndPosition);
+            position.TurnLeft().Should().BeEquivalentTo(expectedNextPosiiton);
         }
 
-        [Test]
-        public void Change_Direction_When_Turining_Right()
+        [TestCaseSource(nameof(TurnRightTestCases))]
+        public void Change_Direction_When_Turning_Right(Position position, Position expectedNextPosiiton)
         {
-            // Arrange
-            var position = new Position(new Point(0, 0), Direction.North);
-            var expectedEndPosition = new Position(new Point(0, 0), Direction.East);
+            position.TurnRight().Should().BeEquivalentTo(expectedNextPosiiton);
+        }
 
-            // Act
-            var nextPosition = position.TurnRight();
+        public static IEnumerable<TestCaseData> TurnLeftTestCases
+        {
+            get
+            {
+                yield return new TestCaseData(new Position(new Point(0, 0), Direction.North), new Position(new Point(0, 0), Direction.West));
+                yield return new TestCaseData(new Position(new Point(0, 0), Direction.South), new Position(new Point(0, 0), Direction.East));
+                yield return new TestCaseData(new Position(new Point(0, 0), Direction.East), new Position(new Point(0, 0), Direction.North));
+                yield return new TestCaseData(new Position(new Point(0, 0), Direction.West), new Position(new Point(0, 0), Direction.South));
+            }
+        }
 
-            // Assert
-            nextPosition.Should().BeEquivalentTo(expectedEndPosition);
+        public static IEnumerable<TestCaseData> TurnRightTestCases
+        {
+            get
+            {
+                yield return new TestCaseData(new Position(new Point(0, 0), Direction.North), new Position(new Point(0, 0), Direction.East));
+                yield return new TestCaseData(new Position(new Point(0, 0), Direction.South), new Position(new Point(0, 0), Direction.West));
+                yield return new TestCaseData(new Position(new Point(0, 0), Direction.East), new Position(new Point(0, 0), Direction.South));
+                yield return new TestCaseData(new Position(new Point(0, 0), Direction.West), new Position(new Point(0, 0), Direction.North));
+            }
         }
     }
 }
